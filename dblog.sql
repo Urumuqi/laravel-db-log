095a63d0cff0c15706f26f8e395520bc30b9a142
wuqi <wuqi226@gmail.com>
2020-07-21 16:20:09 +0800

    update dblog.sql, add index sql

--- dblog.sql
+++ dblog.sql
@@ -7,5 +7,10 @@ CREATE TABLE `db_log` (
   `log_content` json NOT NULL COMMENT '日志内容',
   `created_at` datetime NOT NULL COMMENT '创建时间format:yyyy-mm-dd HH:ii:ss',
   `created_date` datetime NOT NULL COMMENT '创建日期format:yyy-mm-dd',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `idx_big` (`biz_tag`),
+  KEY `idx_action` (`action_tag`),
+  KEY `idx_operator` (`operator`),
+  KEY `idx_track` (`track_key`),
+  KEY `idx_date` (`created_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
